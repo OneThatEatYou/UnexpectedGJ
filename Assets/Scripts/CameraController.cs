0c51b2f93a87eac22904ea9402f53163a63c3b9e
OneThatEatYou <onethateatyou@gmail.com>
2020-09-17 18:12:59 +0800

    added screen shake

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,54 @@
+﻿using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    static CinemachineImpulseSource impulseSource;
+
+    private void Awake()
+    {
+        impulseSource = GetComponent<CinemachineImpulseSource>();
+    }
+
+    private void Update()
+    {
+        //if (Input.GetKeyDown(KeyCode.K))
+        //{
+        //    GenerateImpulse(Vector2.down, 5, 5);
+        //}
+        //else if (Input.GetKeyDown(KeyCode.L))
+        //{
+        //    GenerateImpulse(Vector2.right, 5, 5);
+        //}
+        //else if (Input.GetKeyDown(KeyCode.J))
+        //{
+        //    GenerateImpulse(Vector2.left, 5 ,5);
+        //}
+        //else if (Input.GetKeyDown(KeyCode.I))
+        //{
+        //    GenerateImpulse(Vector2.up, 5 ,5);
+        //}
+    }
+
+    public static void GenerateImpulse(Vector2 direction, float amplitudeGain, float frequencyGain)
+    {
+        impulseSource.m_ImpulseDefinition.m_AmplitudeGain = amplitudeGain;
+        impulseSource.m_ImpulseDefinition.m_FrequencyGain = frequencyGain;
+        impulseSource.m_ImpulseDefinition.m_TimeEnvelope.m_AttackTime = 0.1f;
+        impulseSource.m_ImpulseDefinition.m_TimeEnvelope.m_SustainTime = 0.4f;
+        impulseSource.m_ImpulseDefinition.m_TimeEnvelope.m_DecayTime = 0.9f;
+        impulseSource.GenerateImpulse(direction);
+    }
+
+    public static void GenerateImpulse(Vector2 direction, float amplitudeGain, float frequencyGain, float attackTime, float sustainTime, float decayTime)
+    {
+        impulseSource.m_ImpulseDefinition.m_AmplitudeGain = amplitudeGain;
+        impulseSource.m_ImpulseDefinition.m_FrequencyGain = frequencyGain;
+        impulseSource.m_ImpulseDefinition.m_TimeEnvelope.m_AttackTime = attackTime;
+        impulseSource.m_ImpulseDefinition.m_TimeEnvelope.m_SustainTime = sustainTime;
+        impulseSource.m_ImpulseDefinition.m_TimeEnvelope.m_DecayTime = decayTime;
+        impulseSource.GenerateImpulse(direction);
+    }
+}
--- Assets/Scripts/Robots/Parts/Leg/Redleg.cs
+++ Assets/Scripts/Robots/Parts/Leg/Redleg.cs
@@ -6,6 +6,7 @@ public class Redleg : Leg
 {
     public float moveTime;
     public Vector2 jumpPowerRange;
+    public float gravityScale = 2f;
 
     public override void Action()
     {
@@ -30,12 +31,27 @@ public class Redleg : Leg
         float t = 0;
         Vector2 startPos = Controller.transform.position;
         Vector2 currentPos = startPos;
-        float jumpPower = Random.Range(jumpPowerRange.x, jumpPowerRange.y);
+        //float jumpPower = Random.Range(jumpPowerRange.x, jumpPowerRange.y);     //u
+
+        //while (t != moveTime)
+        //{
+        //    currentPos.x = Mathf.Lerp(startPos.x, target.x, t / moveTime);
+        //    currentPos.y = jumpPower * Mathf.Sin((Mathf.PI) * (t / moveTime));
+        //    Controller.transform.position = currentPos;
+        //    t += Time.deltaTime;
+        //    t = Mathf.Clamp(t, 0, moveTime);
+
+        //    //Debug.Log(t);
+
+        //    yield return null;
+        //}
+
+        float jumpPower = -0.5f * (Physics2D.gravity.y * gravityScale) * moveTime;
 
         while (t != moveTime)
         {
             currentPos.x = Mathf.Lerp(startPos.x, target.x, t / moveTime);
-            currentPos.y = jumpPower * Mathf.Sin((Mathf.PI) * (t / moveTime));
+            currentPos.y = jumpPower * t + 0.5f * (Physics2D.gravity.y * gravityScale) * t * t;
             Controller.transform.position = currentPos;
             t += Time.deltaTime;
             t = Mathf.Clamp(t, 0, moveTime);
@@ -45,6 +61,7 @@ public class Redleg : Leg
             yield return null;
         }
 
+        CameraController.GenerateImpulse(Vector2.down, 5, 5, 0, 0.3f, 0.5f);
         Controller.isMoving = false;
     }
 }

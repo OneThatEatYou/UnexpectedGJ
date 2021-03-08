f3bfc5a57f0f0368801751d9ef5067fbb27f6111
Pang Khang Jer <onethateatyou@gmail.com>
2021-03-08 23:44:11 +0800

    added new player and item spritesheet, shop item scriptable object

---
+++ Assets/Scripts/ScriptableObjects/ShopItem.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewShopItem", menuName = "ScriptableObjects/ShopItem")]
+public class ShopItem : ScriptableObject
+{
+    public string itemName;
+    public Sprite itemSprite;
+    public int itemPrice;
+    [TextArea]
+    public string itemDescription;
+}
---
+++ Assets/Scripts/UI/ShopItemEntry.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ShopItemEntry : MonoBehaviour
+{
+    public ShopItem item;
+    public Image itemImage;
+    public TextMeshProUGUI itemNameText;
+    public TextMeshProUGUI itemPriceText;
+
+    private void OnValidate()
+    {
+        if (item)
+        {
+            if (itemImage && itemNameText && itemPriceText)
+            {
+                itemImage.sprite = item.itemSprite;
+                itemNameText.text = item.itemName;
+                itemPriceText.text = item.itemPrice.ToString();
+            }
+            else
+            {
+                Debug.LogWarning("Shop item entry UI not assigned.");
+            }
+        }
+    }
+}
